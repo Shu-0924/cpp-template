882ac589cecd5aed9ed19fee011da5799507d521
Shu-0924 <71247954+Shu-0924@users.noreply.github.com>
2022-06-20 12:01:07 +0800

    Add files via upload

---
+++ Basic.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int, int>
+#define pis pair<int, string>
+
+#define INF ((ll)1 << 60)
+#define MOD (int)1000000007
+
+#define f first
+#define s second
+#define pb push_back
+
+#define FOR(i, n) for (int (i) = 0; (i) < (int)(n); (i)++)
+#define EACH(x, n) for (auto& (x): (n))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define sz(x) (int)x.size()
+
+//------------------- Basic -------------------//
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    return 0;
+}
